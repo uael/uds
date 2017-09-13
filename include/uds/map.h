f581a4d2b9e53c65e3c485f9d630e7947a226ecb
uael <lucas.abel@yandex.com>
2017-09-13 17:39:40 +0200

    added uds map module template files

--- include/uds/map.h
+++ include/uds/map.h
@@ -23,16 +23,19 @@
  * SOFTWARE.
  */
 
-/*!@file uds.h
+/*!@file uds/map.h
  * @author uael
  */
-#ifndef __UDS_H
-# define __UDS_H
+#ifndef __UDS_MAP_H
+# define __UDS_MAP_H
 
-#include "uds/err.h"
-#include "uds/hash.h"
-#include "uds/map.h"
-#include "uds/math.h"
-#include "uds/vec.h"
+#include <uty.h>
+#include <stdlib.h>
 
-#endif /* !__UDS_H */
+#include "err.h"
+#include "hash.h"
+#include "math.h"
+
+
+
+#endif /* !__UDS_MAP_H */
